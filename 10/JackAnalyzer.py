61c4c7b0319ec4600bb4b794421cea2d0760807e
Julian Mack <julianmack@hotmail.co.uk>
2018-06-13 23:35:35 +0100

    Finished wrangling path-in

--- 10/JackAnalyzer.py
+++ 10/JackAnalyzer.py
@@ -1,3 +1,7 @@
+"""Usage: python JackAnalyzer.py path/file.vm
+    Output: will produce xml parse tree of .jack program
+"""
+
 import sys
 
 def main():
@@ -7,3 +11,29 @@ def main():
         sys.exit(1)
 
     path = sys.argv[1]
+    path = os.path.join(sys.path[0], path)
+
+    #head is parent directory
+    #tail is either directory name or file name
+    head, tail = os.path.split(path) #split path to output directory and filename
+
+    #1) if file:
+    if os.path.isfile(path):
+        if not file_name.endswith(".jack"):
+            print("Usage: python3 JackAnalyzer.py path/file.vm\nfile other than 'xxx.jack' provided")
+            sys.exit(1)
+        else:
+            inf_paths = [path]
+    #2) if directory:
+    elif os.path.isdir(path_in): #directory
+        in_files = os.listdir(path)
+        jack_files = list(filter(lambda x : ".jack" in x, in_files))
+        inf_paths = list(map(lambda x : os.path.join(path, x), jack_files))
+    #3) if neither
+    else:
+        print("Usage: python3 JackAnalyzer.py path/file.jack\nor...\
+        \nUsage: python3 JackAnalyzer.py path/dir")
+        sys.exit(1)
+
+    #create output file path
+     
