6c08b8c71155791385d4a34df0a658ae957b4375
Julian Mack <julianmack@hotmail.co.uk>
2018-06-15 00:23:23 +0100

    is_numeric added

--- 10/helpers.py
+++ 10/helpers.py
@@ -1,11 +1,17 @@
 import string
-print(string.digits)
-print(string.punctuation)
-print(string.alphabetical)
+
 def is_alpha(char):
     """Takes char and returns boolean
     if it is alphanumeric or underscore"""
-    if char.isalpha() or char == "_"
+    if char.isalpha() or char == "_":
+        return True
+    else:
+        return False
 
 def is_symbol(char):
-    
+    punct = string.punctuation.translate(
+    string.punctuation.maketrans("", "", "@!#$£%^_`:?\'\""))
+    return char in punct
+
+def is_numeric(char):
+    return char.isnumeric()
