26037c9f4cca131049e05886ddadf724f1509081
Julian Mack <julianmack@hotmail.co.uk>
2018-06-14 17:38:21 +0100

    Helpers created

---
+++ 10/helpers.py
@@ -0,0 +1,11 @@
+import string
+print(string.digits)
+print(string.punctuation)
+print(string.alphabetical)
+def is_alpha(char):
+    """Takes char and returns boolean
+    if it is alphanumeric or underscore"""
+    if char.isalpha() or char == "_"
+
+def is_symbol(char):
+    
