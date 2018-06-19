f6d7297ac052dd8c459000797a93eb9cc889dea2
Julian Mack <julianmack@hotmail.co.uk>
2018-06-19 13:08:57 +0100

    Symbol table complete and tested

--- 11/CompilationEngine.py
+++ 11/CompilationEngine.py
@@ -5,6 +5,17 @@ IDENTIFIER = "identifier"
 INT_CONST = "integerConstant"
 STRING_CONST = "stringConstant"
 
+#Identifier Types
+STATIC = "static"
+FIELD  = "field"
+ARG    = "argument"
+VAR    = "var"
+THIS   = "this"
+THAT   = "that"
+CLASS  = "class"
+SUBROUTINE = "subroutine"
+
+
 statementTypes = ["let", "if", "while", "do", "return"]
 operators = ["+", "-", "*", "/", "&", "|", "<", ">", "="]
 unaryOperators = ["-", "~"]
@@ -14,7 +25,7 @@ from lxml import etree
 import sys
 
 from helpers import prettify
-
+from SymbolTable import SymbolTable
 
 class CompilationEngine():
     def __init__(self, fp_in, fp_out):
@@ -24,6 +35,8 @@ class CompilationEngine():
         self.total = len(self.tokens)
         self.out_root = etree.Element("class") # root for output
         self.crnt_elem = self.out_root
+        self.symClass = SymbolTable()#create class symbol table
+        self.symSub   = SymbolTable()#create subroutine symbol table
         self.compileClass()
         self.__write_file(fp_out)
 
@@ -33,28 +46,35 @@ class CompilationEngine():
         class className { classVarDec* subroutineDec* }"""
         #add each tkn in turn whilst checking
         #to see if program is valid
+
         self.check_and_add(KEYWORD, "class")
-        self.check_and_add(IDENTIFIER)
+        class_name = self.get()  #classname
+        self.className = class_name.text
+        self.addToXmlTree(class_name)
         self.check_and_add(SYMBOL, "{")
-        while self.check_texts(KEYWORD, ["static", "field"])
+        while self.check_texts(KEYWORD, ["static", "field"]):
             self.compileClassVarDec()
-        while self.check_texts(KEYWORD, ["constructor", "function", "method"])
+        while self.check_texts(KEYWORD, ["constructor", "function", "method"]):
             self.compileSubroutineDec()
         self.check_and_add(SYMBOL, "}")
+        print("class: ")
+        self.symClass.print_table()
 
     def compileClassVarDec(self):
         """ClassVarDec Grammar:
         (static|field) type VarName ("," VarName)* ";" """
         self.addXMLSubElem("classVarDec")
-        self.addToXmlTree(self.get())   #static|field
-        self.addToXmlTree(self.get())   #type
-        self.addToXmlTree(self.get())   #VarName
+        kind_t, type_t, name_t= self.get_mult(3)
+        self.symClass.define(name_t.text, type_t.text, kind_t.text)
+        self.addMultXML([kind_t, type_t, name_t])
         while self.check_texts(SYMBOL, ","): #another VarName
-            self.addToXmlTree(self.get()) # ","
-            self.check_and_add(IDENTIFIER) #VarName(s)
+            comma, name_t = self.get_mult(2)
+            self.symClass.define(name_t.text, type_t.text, kind_t.text)
+            self.addMultXML([comma, name_t])
         self.check_and_add(SYMBOL, ";") #end-VarDec
         #restore parent node
         self.crnt_elem = self.crnt_elem.getparent()
+
 
     def compileSubroutineDec(self):
         """ClassVarDec Grammar:
@@ -62,31 +82,34 @@ class CompilationEngine():
         ("void"| type) subroutineName
         "(" ParameterList ")" SubroutineBody """
         self.addXMLSubElem("subroutineDec")
-
-        self.addToXmlTree(self.get())   #constructor|function|method
-        self.addToXmlTree(self.get())     #"void"| type
-        self.check_and_add(IDENTIFIER)  #subroutineName
-        self.check_and_add(SYMBOL, "(")
+        self.symSub.startSubroutine() #wipe previous sub_vars from Symbol Table
+        tkns = self.get_mult(4)  #constructor|function|method
+        [sub_type, ret_type, sub_name, parenth] = tkns
+        if sub_type.text == "method":
+            self.symSub.define(THIS, self.className, ARG)
+        self.addMultXML(tkns)
         self.compileParameterList()
-        self.check_and_add(SYMBOL, ")")
+        self.check_and_add(SYMBOL, ")") #leave this
         self.compileSubroutineBody()
+        print("sub: ")
+        self.symSub.print_table()
         #restore parent node
         self.crnt_elem = self.crnt_elem.getparent()
 
     def compileParameterList(self):
         """ParameterList Grammar:
         (type varName) ("," type varName)
-        ("void"| type) subroutineName
-        "(" ParameterList ")" SubroutineBody """
+         """
         self.addXMLSubElem("parameterList")
 
         if self.check_texts(KEYWORD):  #if parameter present
-            self.addToXmlTree(self.get()) #type
-            self.check_and_add(IDENTIFIER) #varname
+            type, name = self.get_mult(2)
+            self.symSub.define(name.text, type.text, ARG)
+            self.addMultXML([type, name]) #varname
         while self.check_texts(SYMBOL, ","): #another VarName
-            self.check_and_add(SYMBOL, ",")
-            self.addToXmlTree(self.get()) #type
-            self.check_and_add(IDENTIFIER) #VarName(s)
+            comma, type, name = self.get_mult(3)
+            self.symSub.define(name.text, type.text, ARG)
+            self.addMultXML([comma, type, name]) #varname
 
         #restore parent node
         self.crnt_elem = self.crnt_elem.getparent()
@@ -110,11 +133,13 @@ class CompilationEngine():
         "var" type varName ("," varName)* ";" """
         self.addXMLSubElem("varDec")
         self.check_and_add(KEYWORD, "var")
-        self.addToXmlTree(self.get()) #type
-        self.check_and_add(IDENTIFIER) #varname
+        type, name = self.get_mult(2)
+        self.symSub.define(name.text, type.text, VAR)
+        self.addMultXML([type, name])
         while self.check_texts(SYMBOL, ","): #another VarName
-            self.check_and_add(SYMBOL, ",")
-            self.check_and_add(IDENTIFIER) #VarName(s)
+            comma, name = self.get_mult(2)
+            self.symSub.define(name.text, type.text, VAR)
+            self.addMultXML([comma, name])
         self.check_and_add(SYMBOL, ";")
         #restore parent node
         self.crnt_elem = self.crnt_elem.getparent()
@@ -148,7 +173,10 @@ class CompilationEngine():
         "=" expression ";" """
         self.addXMLSubElem("letStatement")
         self.addToXmlTree(self.get()) #let
-        self.check_and_add(IDENTIFIER) #varname
+        self.check_and_add(IDENTIFIER) #varName
+        #kind=KindOf(varName)
+        #index = indexOf(varName)
+        #use these for vmLet()
         if self.check_texts(SYMBOL, "["): #nested expression
             self.addToXmlTree(self.get())  #"["
             self.compileExpression()
@@ -249,16 +277,17 @@ class CompilationEngine():
             self.check_and_add(SYMBOL, ")")
         elif self.check_texts(IDENTIFIER):
             self.addToXmlTree(tkn, True) #i.e. now test next tkn
-            if self.check_texts(SYMBOL, "["): #varname "[" expression "]"
-                self.addToXmlTree(self.get()) #"["
+            if self.check_texts(SYMBOL, "["): #array
+                #access info from symbol table
+                self.addToXmlTree(self.get()) #varname "[" expression "]"
                 self.compileExpression()
                 self.check_and_add(SYMBOL, "]")
-            elif self.check_texts(SYMBOL, "("): #subroutineName "(" expressionList ")"
-                self.addToXmlTree(self.get())
+            elif self.check_texts(SYMBOL, "("): #function call
+                self.addToXmlTree(self.get()) #subroutineName "(" expressionList ")"
                 self.compileExpressionList()
                 self.check_and_add(SYMBOL, ")")
-            elif self.check_texts(SYMBOL, "."): #(className|varName) "." subroutineName"(" expressionList ")"
-                self.addToXmlTree(self.get())
+            elif self.check_texts(SYMBOL, "."): #method call
+                self.addToXmlTree(self.get())  #(className|varName) "." subroutineName"(" expressionList ")"
                 self.check_and_add(IDENTIFIER) #subroutineName
                 self.check_and_add(SYMBOL, "(")
                 self.compileExpressionList()
@@ -321,7 +350,9 @@ class CompilationEngine():
         child.text = tkn.text
         if increment:
             self.num +=1
-
+    def addMultXML(self, xs):
+        for x in xs:
+            self.addToXmlTree(x)
     def addXMLSubElem(self, name):
         """Adds new sub-elem to tree"""
         NewChild = etree.SubElement(self.crnt_elem, name)
@@ -339,6 +370,11 @@ class CompilationEngine():
         else:
             return None
 
+    def get_mult(self, n):
+        tkns = []
+        for i in range(n):
+            tkns.append(self.get())
+        return tkns
     def fault(self):
         """called if incorrect program provided
         make this error message more expressive"""
@@ -360,4 +396,4 @@ class CompilationEngine():
 
 if __name__ == "__main__":
     #tests
-    compiler = CompilationEngine("expressionlessSquare/mainT.xml", "hi")
+    compiler = CompilationEngine("../10/square/mainT.xml", "hi")
