d85687d90b7c2b2f979678b3ed6f5a10bc04a56b
Julian Mack <julianmack@hotmail.co.uk>
2018-06-19 13:09:26 +0100

    Symbol table complete and tested

--- 11/SymbolTable.py
+++ 11/SymbolTable.py
@@ -1,3 +1,41 @@
+#dictionary keywords
+KIND = "kind"
+TYPE = "type"
+INDEX = "index"
+SCOPE = "scope"
+
+
 class SymbolTable():
+    """Symbols are held in nested dictionary of the form:
+    {(var_name): {kind: "...", type: "...", index: "..."}}
+    e.g: {x: {kind: "static", type: "int", index: "0"}, ...}
+    """
     def __init__(self):
-        
+        self.symbols = {}
+
+    def define(self, name, type, kind):
+        index = self.varCount(kind)
+        #add to dict - use name as key
+        self.symbols[name] = {KIND: kind, TYPE: type, INDEX: index}
+
+    def varCount(self, kind):
+        count = 0
+        for entry in self.symbols.values():
+            if entry[KIND] == kind:
+                count +=1
+        return count
+
+    def KindOf(self, name):
+        return self.symbols[name][KIND]
+    def TypeOf(self, name):
+        return self.symbols[name][TYPE]
+    def IndexOf(self, name):
+        return self.symbols[name][INDEX]
+
+    def startSubroutine(self):
+        """Wipe all entries from table"""
+        self.symbols = {}
+
+    def print_table(self):
+        for key, value in self.symbols.items():
+            print(key, ": ", value)
